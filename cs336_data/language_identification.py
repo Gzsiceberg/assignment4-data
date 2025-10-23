4bdcec96fcf5d88052c842e98fb5f044115d83da
Iceberg <gzs_iceberg@outlook.com>
2025-10-23 13:43:20 +0800

    Refactor language detection script to improve readability and functionality

--- cs336_data/language_identification.py
+++ cs336_data/language_identification.py
@@ -1,4 +1,6 @@
 import fasttext
+
+from cs336_data.extract_text import extract_warc
 def detect_language(text: str) -> tuple[str, float]:
     text = text.replace("\n", " ").strip()
     model = fasttext.load_model("model/lid.176.bin")
@@ -9,10 +11,32 @@ def detect_language(text: str) -> tuple[str, float]:
 
 
 if __name__ == "__main__":
-    # sample_text = "This is a sample English text."
-    # lang, confidence = detect_language(sample_text)
-    # print(f"Detected language: {lang} (Confidence: {confidence})")
+    sample_text = "Bonjour tout le monde"
+    lang, confidence = detect_language(sample_text)
+    print(f"Detected language: {lang} with confidence {confidence}")
 
-    smaple_text = 'Herman Melville - Moby-Dick\n\nAvailing himself of the mild, summer-cool weather that now reigned in these latitudes,...airing, or new shaping their various weapons and boat furniture.\n\nThis is a test paragraph with a link.\n\n  • Novel'
-    lang, confidence = detect_language(smaple_text)
-    print(f"Detected language: {lang} (Confidence: {confidence})")
+    from fastwarc.warc import ArchiveIterator, WarcRecordType
+    from rich.progress import track
+    warc_path = "data/CC-MAIN-20250417135010-20250417165010-00065.warc.gz"
+    count = 0
+    is_english = 0
+    total_records = 0
+    with open(warc_path, "rb") as f:
+        for i, record in enumerate(track(ArchiveIterator(f))):
+            if record.record_type != WarcRecordType.response:
+                continue
+            payload = record.reader.read()
+            text = extract_warc(payload)
+            lang, confidence = detect_language(text)
+            total_records += 1
+            if lang == "en":
+                is_english += 1
+            if count < 20:
+                print("-" * 80)
+                print(f"RecordID: {record.record_id} - Detected language: {lang} with confidence {confidence}")
+                text = text.replace("\n", " ")
+                print(text[:200])
+                count += 1
+            if total_records >= 10000:
+                break
+    print(f"Total records: {total_records}, English records: {is_english} percentage: {is_english / total_records:.2%}")
